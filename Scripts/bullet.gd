d11dc13fb541de90f77e4060a87ed90d9e180a0d
mmurrellONU <m-murrell@onu.edu>
2025-10-25 15:43:23 -0400

    Fixed Shooting and added Wing Animations

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
-var speed = -100.0
+var speed = 800.0
 
 func _physics_process(delta):
 	position += transform.x * speed * delta
+
+func _on_area_entered(area: Area2D) -> void:
+	if(area.is_in_group("Boundary")):
+		queue_free()
